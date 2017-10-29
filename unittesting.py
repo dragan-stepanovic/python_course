5925d4860b093c5826a31ad9febdffd2d01c6dd7
dragan.stepanovic <dragan.stepanovic@nultien.rs>
2017-10-29 22:57:45 +0100

    we can now count total number of characters

--- unittesting.py
+++ unittesting.py
@@ -4,7 +4,7 @@ import unittest
 class FileParserTests(unittest.TestCase):
 
     def test_returns_sum_of_all_file_lines(self):
-        lines = ['first line', 'second line', 'third line']
+        lines = ['first line', 'second line', 'third line is this']
         lines_count = FileParser().parse(lines)[0]
         self.assertEqual(lines_count, 3)
 
@@ -12,6 +12,11 @@ class FileParserTests(unittest.TestCase):
         lines = ['first line', 'second line', 'third line is this']
         word_count = FileParser().parse(lines)[1]
         self.assertEqual(word_count, 8)
+
+    def test_returns_number_of_characters(self):
+        lines = ['first line', 'second line', 'third line is this']
+        char_count = FileParser().parse(lines)[2]
+        self.assertEqual(char_count, 39)
 
 
 class FileParser:
@@ -23,7 +28,14 @@ class FileParser:
         #     for word in line.split():
         #         words.append(word)
 
-        return len(lines), len(words)
+        characters = [char for line in lines for char in list(line)]
+        # same as
+        # characters = []
+        # for line in lines:
+        #     for character in list(line):
+        #         characters.append(character)
+
+        return len(lines), len(words), len(characters)
 
 
 if __name__ == '__main__':
