560dec26d79f514ecb0909a1448a96b3b8247e18
dragan.stepanovic <dragan.stepanovic@nultien.rs>
2017-10-29 22:33:24 +0100

    test for returning number of words added and implemented

--- unittesting.py
+++ unittesting.py
@@ -5,13 +5,25 @@ class FileParserTests(unittest.TestCase):
 
     def test_returns_sum_of_all_file_lines(self):
         lines = ['first line', 'second line', 'third line']
-        count = FileParser().parse(lines)
-        self.assertTrue(count == 3)
+        lines_count = FileParser().parse(lines)[0]
+        self.assertEqual(lines_count, 3)
+
+    def test_returns_number_of_words(self):
+        lines = ['first line', 'second line', 'third line is this']
+        word_count = FileParser().parse(lines)[1]
+        self.assertEqual(word_count, 8)
 
 
 class FileParser:
     def parse(self, lines):
-        return len(lines)
+        words = [word for line in lines for word in line.split()]
+        # same as
+        # words = []
+        # for line in lines:
+        #     for word in line.split():
+        #         words.append(word)
+
+        return len(lines), len(words)
 
 
 if __name__ == '__main__':
